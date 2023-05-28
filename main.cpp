5e5ed2c214b5893bcb5d38273359574890247919
Seyyed Mohammad Saleh Mostafei <131899691+mohammadsaleh2002@users.noreply.github.com>
2023-05-28 10:13:47 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-/*
+	/*
 
-	BillBoard	:	This program receives the text from the user and moves it in 8 directions.
+		BillBoard	:	This program receives the text from the user and moves it in 8 directions.
 
-*/
+	*/
 
 #include <iostream>
 #include <windows.h>
@@ -12,16 +12,18 @@ using namespace std;
 
 static int width, height;
 
-/* Variable to check for hitting the console wall */
+	/* Variable to check for hitting the console wall */
 
 static bool hit;
 
-/*  move Up or Down */
+	/*  move Up or Down */
 
-void move_UpDown(string text, int& x, int& y, int& dirY) {
+void move_UpDown(string text, int& x, int& y, int& dirY) 
+{
     y += dirY;   
-/* check for hit up and down wall */
-    if(y < 1 || y > height) {
+	/* check for hit up and down wall */
+    if(y < 1 || y > height ) 
+	{
         dirY *= -1;     
         hit = true;
         return;
@@ -30,15 +32,18 @@ void move_UpDown(string text, int& x, int& y, int& dirY) {
     COORD pos = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
     cout << text;
+	Sleep(50);
 
 }
 
-/*  move Left or Right */
+	/*  move Left or Right */
 
-void move_LeftRight(string text, int& x, int& y, int& dirX) {
+void move_LeftRight(string text, int& x, int& y, int& dirX) 
+{
     x += dirX;  
     /* check for hit right and left wall */    
-    if(x < 1 || x+text.length() > width - 1 ) {
+    if(x < 1 || x+text.length() > width ) 
+	{
         dirX *= -1;   
         hit = true;
         return;
@@ -47,29 +52,33 @@ void move_LeftRight(string text, int& x, int& y, int& dirX) {
     COORD pos = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
     cout << text;
-
+	Sleep(50);
 }
 
 	/*  move Diagonally */
 
-void move_Diagonally(string text, int& x, int& y, int& dirX, int& dirY) {
+void move_Diagonally(string text, int& x, int& y, int& dirX, int& dirY) 
+{
     x += dirX;
     y += dirY;
 	/* check for hit corners */
-	if((x < 1 || x + text.length() > width - 1 ) && (y < 1 || y > height - 1)) {
+	if((x < 1 || x + text.length() > width ) && (y < 1 || y > height )) 
+	{
 		dirY *= -1;
         dirX *= -1;
         hit = true;
         return;
     }
     /* check for hit right and left wall */
-    if(x < 1 || x + text.length() > width - 1) {
+    if(x < 1 || x + text.length() > width ) 
+	{
         dirX *= -1;
         hit = true;
         return;
     }
     /* check for hit up and down wall */
-    if(y < 1 || y > height ){
+    if(y < 1 || y > height )
+	{
     	dirY *= -1;
     	hit = true;
     	return;
@@ -78,68 +87,67 @@ void move_Diagonally(string text, int& x, int& y, int& dirX, int& dirY) {
     COORD pos = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
     cout << text;
+	Sleep(50);
 
 }
 
-int main(){
+int main()
+{
 
     string input ;
-    int delayTime;
     
     int x = 0, y = 0;
     int dirX = 1, dirY = 1;
 
 
-    /* Adjust console size */
+    /*  Adjust console size 
+    
+		copied from stackoverflow
+	*/
     
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    
-    /* The width of the console */
-    
+	 
     width = csbi.srWindow.Right - csbi.srWindow.Left ;
-    
-    /* The height of the console */
     
     height = csbi.srWindow.Bottom - csbi.srWindow.Top ;
 
-	while(true) {
+	while(true) 
+	{
 		
-    	    cout << "Enter something : " ;
+    	    cout << "Enter something (less than " << width / 5 << " charachters): " ;
         	getline(cin , input);
-        	if (input.length() > width/3) {
-            	cout << "Error: Input is too long. Please enter a shorter string." << endl;
+        	if (input.length() > width / 5) 
+			{
+            	cout << "Error: Input is too long. Please enter a shorter string." << endl ;
             	continue;
         	}
-        	
-        	cout << "Enter delay time (in milliseconds): ";
-        	cin >> delayTime;
+     
         	
 		srand(time(0));
-    		while(true) {
-			int r = rand() % 3;
-    			while(!hit){
-	    			switch(r){
-	    				case 0:
-	    					move_UpDown(input, x, y, dirY);
-	    					Sleep(delayTime);
-	    					break;
-	    				case 1:
-						move_Diagonally(input, x, y, dirX, dirY);
-						Sleep(delayTime);
-						break;
-	    				case 2:
-		    				move_LeftRight(input, x, y, dirX);
-		    				Sleep(delayTime);
-	    					break;
-					}
-					system("cls");
+    	while(true) 
+		{
+		int r = rand() % 4;
+    		while(!hit)
+			{
+	    		switch(r)
+				{
+	    			case 0:
+	    				move_UpDown(input, x, y, dirY);
+	    				break;
+	    			case 1:
+	    			case 2:
+					move_Diagonally(input, x, y, dirX, dirY);
+					break;
+	    			case 3:
+	    				move_LeftRight(input, x, y, dirX);
+	    				break;
 				}
-				if(hit){
+				system("cls");
+			}
+			if(hit)
 				hit = false;
-				}
-			
-    		}
-    		return 0;
+    	}
 	}
+	return 0;
 }
